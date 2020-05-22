3dcffee7c1b0d83d0b9e38fae8b74790dc2ad48d
Farhan-Khalifa-Ibrahim <farhankhalifa1510@gmail.com>
2020-05-23 00:55:38 +0800

    updated my word

--- src/pages/App.js
+++ src/pages/App.js
@@ -15,17 +15,15 @@ class App extends React.Component {
         <Container fluid="md" className="inside">
           <Row>
             <Col md="6">
-              Hi everyone welcome to my blog, my name is Farhan Khalifa
-              Ibrahim. I will be sharing about books and software engineer here.
-              I am still a newbie in many things around software engineer that
-              is why I would like to invite you to have a slack group for us to
-              discuss software engineer, you can share information or ask
-              questions. This group will be focusing on five aspects (Frontend,
-              Backend, Mobile Development, Data Structures and Algorithm, System
-              Design). If you are interested, you can fill the form in
-              <a href="https://forms.gle/qZMRF3JwDs7J9rih8">here</a>. I will
-              only invite you if I know you personally, or you are a friend of
-              my friend (This is to prevent any toxic members). I will not
+              Hi everyone welcome to my blog, my name is Farhan Khalifa Ibrahim.
+              I will be sharing about books and software engineer here. I am
+              still a newbie in many things around software engineer that is why
+              I would like to invite you to have a slack group for us to discuss
+              software engineer, you can share information or ask questions.
+              This group will be focusing on five aspects (Frontend, Backend,
+              Mobile Development, Data Structures and Algorithm, System Design).
+              If you are interested, you can fill the form in
+              <a href="https://forms.gle/qZMRF3JwDs7J9rih8">here</a>. I will not
               accept randomly, although we know each other.
               <br />
               <br />I want to have a group because I believe that everyone has
@@ -33,10 +31,10 @@ class App extends React.Component {
               hope you can find a friend who understands your questions very
               well. From my own personal problem, I have a very close mentor who
               already experienced in programming for almost four years. Still,
-              there are many things he doesn't know about usually because the
-              stack he uses is different from my questions. Also, I am more
-              comfortable asking a person if I know him very well, or else I
-              would better ask in a forum.
+              there are many things he doesn't know about because the stack he
+              uses is different from my questions. Also, I am more comfortable
+              asking a person if I know him very well, or else I would better
+              ask in a forum such as slack group.
             </Col>
           </Row>
         </Container>
